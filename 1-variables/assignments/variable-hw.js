8674a2f1dc1c73e6ccc42b168b46f8737e9c537f
Asadur Rahman <asadrm63@gmail.com>
2023-03-07 15:56:42 -0500

    variable-hw

--- 1-variables/assignments/variable-hw.js
+++ 1-variables/assignments/variable-hw.js
@@ -4,6 +4,35 @@
 
 
 //1. Create a set of variables that define an animal (age, height, name, etc...). Must be at least 6 variables.
-const age = 21
+
+const age = 4
+const height = "2 feet"
+const nameAnimal = "Percy"
+const isActive = true 
+const eyeColor = "animal"
+const animalType = "cat"
+
+console.log(age)
+console.log(height)
+console.log(nameAnimal)
+console.log(isActive)
+console.log(eyeColor)
+console.log(animalType)
+
+
 
 //2. Create a set of variables that define a vehicle. Must be at least 6 variables.
+
+const color = 'red'
+const isRunning = true
+const model = "M3"
+const brand ="BMW"
+const engine = "v6tt"
+const yearMade = 2020
+
+console.log(color)
+console.log(isRunning)
+console.log(model)
+console.log(brand)
+console.log(engine)
+console.log(yearMade)
