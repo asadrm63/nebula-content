a95bf05287c4abcda81cd1374911c53c416fdabb
Asadur Rahman <asadrm63@gmail.com>
2023-03-08 10:50:28 -0500

    numbers-practice

---
+++ 2-numbers/practice/numbers-1.js
@@ -0,0 +1,40 @@
+
+// Declare a variable and assign it to a numeric value
+// Double that value and re-assign it to the previous variable
+
+let num =12
+num *=2
+console.log(num)
+
+
+
+// Create two numbers, assign them to variables and add them together
+const num1 = 22
+const num2 = 11
+console.log(num1 +num2)
+ 
+// Create three numbers, assign them to variables and subtract the smaller two from the largest
+const num3 = 2
+const num4 = 5
+const num5 =10
+console.log(num5-(num4+num3))
+
+
+// Create two even numbers, assign them to variables and divide one from the other
+const even1 = 24
+const even2 = 8
+console.log(even1/even2)
+
+
+// Create two odd numbers, assign them to variables and multiply one from the other
+
+const odd1 = 3
+const odd2 = 7
+console.log(odd1*odd2)
+
+// Create a variable and log it's remainder, if divided by 3, to the console 
+const students = 17
+console.log(students%3)
+
+// Answer in plain text, what is modulo especially useful for?
+// It is especially  useful to find out if a number is even or odd.
---
+++ 2-numbers/practice/numbers-2.js
@@ -0,0 +1,48 @@
+
+// For these questions use the Math methods that JavaScript includes
+// All Math methods: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+
+
+// find the square root of 25
+let sqRt = Math.sqrt(25)
+console.log(sqRt)
+
+// find the value of 3 to the power of 5
+let power = Math.pow(3,5)
+console.log(power)
+
+
+// find the value of 7.4 rounded
+let rounded = Math.round(7.4)
+console.log(rounded)
+
+
+// find the rounded-up value of 7.4
+
+let roundedUp = Math.ceil(7.4)
+console.log(roundedUp)
+
+
+// find the absoloute value of -13
+let absoloute = Math.abs(-13)
+console.log(absoloute)
+
+
+
+// find the minimum value in a set of numbers: 12,13,14,15,2,-1
+let minVal = Math.min(12,13,14,15,2,-1)
+console.log(minVal)
+
+
+// find the maximum value in a set of numbers: 12,13,14,15,2,-1\
+let maxVal = Math.max(12,13,14,15,2,-1)
+console.log(maxVal)
+
+
+// create a random number 
+let randNum = Math.random()
+console.log(randNum)
+
+// Create a random number between 0-10
+let randLim = Math.floor(Math.random()*10)
+console.log(randLim)
