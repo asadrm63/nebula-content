03b5ecece1d54415097130a1a245f5f70fbeab0b
Asadur Rahman <asadrm63@gmail.com>
2023-04-27 08:58:54 -0400

    as

--- 16-regex/assignments/Regex-practice.js
+++ 16-regex/assignments/Regex-practice.js
@@ -1,6 +1,11 @@
 // 1.   Write a function that tests if a string contains the string of '123' within it
 let str ="asad 123"
-console.log(str.test(/123/))
+// console.log(str.test(/123/))
+
+function contains123(str){
+    const regex = /123/
+    return regex.test(str)
+}
 
 // 2.   Write a function that tests if a string contains the letter 'a' then the letter 'c' at any point afterward
 //      ex: 'abc' => true
@@ -11,6 +16,9 @@ console.log(str.test(/123/))
 //      ex: 'ca'  => false
 console.log(str.test(/a.*c/))
 
+const testCafterA =(string) {
+    return /a_ i.test(string)
+}
 // 3.   Write a function that reports how many instances of a given string there are 
 //      This function should take in a string and a substring to look for 
 //      Input: 'this is a test', 't'
